1e75f2f4d9817707fa490b76cbab1f8cff8d2f2a
sudoping01 <sudoping01@gmail.com>
2025-09-24 16:29:32 +0000

    feat: vits models dataclasses

---
+++ maliba_ai/settings/tts/malian_tts.py
@@ -0,0 +1,23 @@
+
+from typing import List
+
+class Submodels:
+    bambara = "bambara"
+    boomu = "boomu"
+    dogon = "dogon"
+    pular = "pular"
+    songhoy = "songhoy"
+    tamasheq = "tamasheq"
+    all_languages = [bambara, boomu, dogon, pular, songhoy, tamasheq]
+
+
+class Settings:
+    models_repo: str = "MALIBA-AI/malian-tts"
+    models: List[str] = Submodels.all_languages
+    models_subfolder: str = "models"
+
+
+    
+
+
+
