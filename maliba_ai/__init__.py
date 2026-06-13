4e6aefb62ca8e04b81eee70786230e8340d659eb
sudoping01 <sudoping01@gmail.com>
2026-06-13 21:16:36 +0000

    Expose top-level package API

--- maliba_ai/__init__.py
+++ maliba_ai/__init__.py
@@ -1,2 +1,13 @@
 __version__ = "v2.0.0-alpha"
 __author__ = "sudoping01"
+
+from maliba_ai.core import (LLM, Embeddings, MachineTranslation,
+                            SpeechRecognition, TTS)
+
+__all__ = [
+    "SpeechRecognition",
+    "MachineTranslation",
+    "Embeddings",
+    "LLM",
+    "TTS",
+]
